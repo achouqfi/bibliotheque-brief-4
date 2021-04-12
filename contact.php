645f5500790f70c1ae3f7934210fb067f01993e2
ayoubmehd <safi-_-gio-raja@live.fr>
2021-04-12 23:37:06 +0000

    feat: style contact and home

--- contact.php
+++ contact.php
@@ -11,49 +11,48 @@
 
 ?>
 
+<main class="contact">
 
     <!--   contact     -->
-    <div class="tittle">
+    <div class="separation">
         <h1>CONTACT</h1>
+    </div>
+    <form class="formulaire-msg">
+        <div class="col">
+            <div class="form-fild">
+                <input id="name" type="text" placeholder="nom*">
+                <div id="mName" class="errors"> </div>
+            </div>
+            <div class="form-fild">
+                <input id="email" type="email" placeholder="email*" required>
+                <div id="mEmail" class="errors"> </div>
+            </div>
         </div>
-        <div  class="formulaire-msg">
-            <div class="contact1">
-                <div class="placeh">
-                    <input id="name" type="text" placeholder="nom*" >
-                    <div id="mName" class="errors"> </div>
-                </div>
-                <div>
-                    <input id="email" type="email" placeholder="email*" required>
-                    <div id="mEmail" class="errors"> </div>
-                </div>
+        <div class="col">
+            <div class="form-fild">
+                <input id="phone" placeholder="telephone*">
+                <div id="mPhone" class="errors"> </div>
             </div>
-            <div class="contact1">
-                <div>
-                    <input id="phone" placeholder="telephone*">
-                    <div id="mPhone" class="errors"> </div>
-                </div>
-                <div>
-                    <input id="subject" placeholder="sujet*">
-                    <div id="mSujet" class="errors">  </div>
-    
-                </div>
+            <div class="form-fild">
+                <input id="subject" placeholder="sujet*">
+                <div id="mSujet" class="errors"> </div>
+
             </div>
-            <div class="contact3">
-                <div>
-                <textarea placeholder="Message*" id="message"></textarea>
-                <div id="mMessage" class="errors">  </div>
-                </div>
-            </div>
-            <div class="evnvy" style="padding-bottom: 5%;">
-                <button onclick="validateForm()" >Envoyer</button>
-            </div> 
         </div>
-        
-    
+        <div class="col">
+            <textarea placeholder="Message*" id="message"></textarea>
+            <div id="mMessage" class="errors"></div>
+        </div>
+        <div class="col">
+            <button class="cta" onclick="validateForm()">Envoyer</button>
+        </div>
+    </form>
+
+</main>
+
 
 <?php
 
     require_file("/includes/footer.php");
     
 ?>
-
--- includes/nav.php
+++ includes/nav.php
@@ -2,11 +2,11 @@
     <div class="overlay"></div>
     <div class="nav-left">
         <div class="logo">
-        <img src="<?php p_base_url("/img-books/logo.svg") ?>" alt="logo" />
+            <img src="<?php p_base_url("/img-books/logo.svg") ?>" alt="logo" />
         </div>
     </div>
     <ul class="nav-menu">
-        <li><a href="<?php p_base_url("/index,php") ?>">Home</a></li>
+        <li><a href="<?php p_base_url("/index.php") ?>">Home</a></li>
         <li><a href="<?php p_base_url("/gallery.php") ?>">Gallery</a></li>
         <li><a href="<?php p_base_url("/authors.php") ?>">Authors</a></li>
         <li><a href="<?php p_base_url("/books.php") ?>">Books</a></li>
